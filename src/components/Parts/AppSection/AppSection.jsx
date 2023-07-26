e319e10d1e4b40278fe5b4d60a060498d2ace46b
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-07-26 12:48:40 -0700

    working on styling

--- src/components/Parts/AppSection/AppSection.jsx
+++ src/components/Parts/AppSection/AppSection.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import '../AppSection/AppSection.css'
 import { ReactComponent as Goggle } from "../../assets/WGoggle.svg";
 import { ReactComponent as PlayStore } from "../../assets/Wplaystore.svg";
+import { ReactComponent as Car } from "../../assets/car.svg";
+import { ReactComponent as PayStack } from "../../assets/pay-stark.svg";
+import { ReactComponent as Gift} from "../../assets/gift.svg";
+import { ReactComponent as Call} from "../../assets/call.svg";
 import Wtrolley from '../../assets/W trolley.webp'
 import womanapp from '../../assets/woman app.webp'
 
 
 function AppSection() {
   return (
-      <div className='AppSection Py5'>
+    <>
+      <section className='AppSection Py5'>
         <div>
         <img className='appstore-img fir-img' src={ Wtrolley}  alt='picture'  />
         </div>
@@ -25,7 +30,31 @@ function AppSection() {
         <div>
         <img className='appstore-img sec-img' src={womanapp}  alt=''  />
         </div>
-      </div>
+      </section>
+
+       <section className='contacte-container'>
+          <div className='cover-info'>
+              <Car className='cover-img'/>
+              <p className='cover-text'>Free Shipping From $500.00</p>
+          </div>
+          <div className='cover-info'>
+              <Call  className='cover-img'/>
+              <p className='cover-text'>Support 24/7 At Anytime</p>
+          </div>
+          <div className='cover-info'>
+          <PayStack  className='cover-img'/>
+          <p className='cover-text'>Secure Payment Totally Safe</p>
+          </div>
+          <div className='cover-info'>
+            <Gift className='cover-img'/>
+            <p className='cover-text'>Latest Offer Upto 20% Off</p>
+          </div>
+        </section> <hr></hr>
+
+        <section>
+          
+        </section>
+      </>
   )
 }
 
