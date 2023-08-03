bfb87caf560374bda5b7ccb8678b5714a70c52ae
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-08-02 23:32:52 -0700

    corrections invoiceform

--- src/components/Useables/OrderDetails/OrderDetailsBox.jsx
+++ src/components/Useables/OrderDetails/OrderDetailsBox.jsx
@@ -44,32 +44,33 @@ function OrderDetail({ invoices }) {
   return (
     <div className="main-orderdetail">
       <div className="orderdetail-sec1">
-        <button className="print-button" onClick={handlePrint}>
-          Print Receipt
-        </button>
+      <button className="print-button" onClick={handlePrint}>
+        Print Receipt
+      </button>
         <h2>INVOICE</h2>
-        <div className="cypher-imgdiv">
-          <Cypher className="cypher-img" />
-          <p>Lafe Akure</p>
-        </div>
-        <div>
-          <h3>Date</h3>
-          <span>{formattedDate}</span>
-        </div>
+      <div className="cypher-imgdiv">
+        <Cypher className="cypher-img"/>
+        <p>Lafe Akure</p>
+      </div>
+      <div>
+        <h3>Date</h3>
+        <span>{formattedDate}</span>
+      </div>
 
-        <div>
-          <h3>Invoice No.</h3>
-          <p>#{invoice}</p>
-        </div>
+      <div>
+        <h3>Invoice No.</h3>
+        <p>#{invoice}</p>
+      </div>
 
-        <div>
-          <h3>Invoice To.</h3>
-          <p>{senderName}</p>
-          <p>{address}</p>
-          <p>{city}</p>
-          <p>{email}</p>
-          <p>{number}</p>
-        </div>
+      <div>
+        <h3>Invoice To.</h3>
+        <p>{senderName}</p>
+        <p>{address}</p>
+        <p>{city}</p>
+        <p>{email}</p>
+        <p>{number}</p>
+      </div>
+
       </div>
 
       <div className="order-scroll userOrdersPage">
@@ -83,7 +84,7 @@ function OrderDetail({ invoices }) {
               <th>Amount</th>
             </tr>
           </thead>
-          {items.map((order, index) => {
+          {items?.map((order, index) => {
             console.log(order, "ooorr");
             const { status, name, count, price } = order;
             return (
@@ -103,20 +104,20 @@ function OrderDetail({ invoices }) {
 
       <div className="orderdetail-sec2">
         <div>
-          <h4>Discount</h4>
-          <p style={{ color: " #fb923c" }}>${discountPrice}</p>
-        </div>
-
-        <div>
-          <h4>Total Item Price</h4>
-          <p style={{ color: "#20c997" }}>${totalPriceInCart}</p>
-        </div>
-
-        <div>
-          <h4>Total Amount</h4>
-          <p style={{ color: "#FF0000" }}>${totalPriceToPay}</p>
-        </div>
+        <h4>Discount</h4>
+        <p style={{ color: " #fb923c" }}>${discountPrice}</p>
       </div>
+
+      <div>
+        <h4>Total Item Price</h4>
+        <p style={{color:'#20c997'}}>${totalPriceInCart}</p>
+      </div>
+
+      <div>
+        <h4>Total Amount</h4>
+        <p style={{color:'#FF0000'}}>${totalPriceToPay}</p>
+      </div>
+    </div>
     </div>
   );
 }
