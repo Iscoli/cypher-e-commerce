9284d602e7144856ed7647575ae41fd9b69fbbe2
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-08-30 15:35:23 -0700

    about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1>ABOUT Us</h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem vitae quibusdam alias quaerat? Natus similique nam eligendi hic ratione, sequi beatae ex facere assumenda sed consectetur animi! Dignissimos voluptatibus non animi quasi aperiam dolorem incidunt exercitationem consequatur, assumenda modi suscipit alias nam, earum fugiat nesciunt corrupti voluptas ipsam officia cupiditate ullam sequi. Maxime eos necessitatibus adipisci architecto temporibus reprehenderit incidunt iste. Libero assumenda adipisci dignissimos quo ratione, repudiandae eligendi accusamus?
+      </h1>
     </div>
   )
 }
--- src/components/Useables/Pages.jsx
+++ src/components/Useables/Pages.jsx
@@ -1,74 +1,75 @@
- import React from 'react'
- import { Link } from 'react-router-dom';
- import  '../UsablesCss/Pages.css';
- import { ReactComponent as Offer } from '../assets/offer.svg';
- import { ReactComponent as FAQ } from '../assets/faqe.svg';
- import { ReactComponent as NotFound } from '../assets/404e.svg';
- import { ReactComponent as Privacy} from '../assets/privacy.svg';
- import { ReactComponent as Contact } from '../assets/contact.svg';
- import { ReactComponent as Check } from '../assets/check.svg';
- import { ReactComponent as About } from '../assets/about.svg';
- import { ReactComponent as Terms } from '../assets/tems.svg';
- function Pages({closeModalHandler}) {
+import React from "react";
+import { Link } from "react-router-dom";
+import "../UsablesCss/Pages.css";
+import { ReactComponent as Offer } from "../assets/offer.svg";
+import { ReactComponent as FAQ } from "../assets/faqe.svg";
+import { ReactComponent as NotFound } from "../assets/404e.svg";
+import { ReactComponent as Privacy } from "../assets/privacy.svg";
+import { ReactComponent as Contact } from "../assets/contact.svg";
+import { ReactComponent as Check } from "../assets/check.svg";
+import { ReactComponent as About } from "../assets/about.svg";
+import { ReactComponent as Terms } from "../assets/tems.svg";
+function Pages({ closeModalHandler }) {
   const PagesItem = [
     {
       path: "/offer",
       name: "Offer",
-      icon: <Offer/>
+      icon: <Offer />,
     },
     {
       path: "/check",
       name: "Checkout",
-      icon: <Check/>
+      icon: <Check />,
     },
 
     {
       path: "/comment",
       name: "FAQ",
-      icon: <FAQ/>
+      icon: <FAQ />,
     },
     {
       path: "/about",
       name: "About US",
-      icon: <About/>
+      icon: <About />,
     },
     {
       path: "/product",
       name: "Contact US",
-      icon: <Contact/>
+      icon: <Contact />,
     },
     {
       path: "/product",
       name: "Privacy Policy",
-      icon: <Privacy/>
+      icon: <Privacy />,
     },
     {
       path: "/product",
       name: "Terms & Condition",
-      icon: <Terms/>
+      icon: <Terms />,
     },
     {
       path: "/product",
       name: "404",
-      icon: <NotFound/>
+      icon: <NotFound />,
     },
   ];
-   return (
-     <div>
-       
-       {PagesItem.map((item, index) => (
-            
-              <Link className='linke' key={index} to={`${item.path}`} onClick={closeModalHandler}>
-                  <div  className='pages-moda'>
-                    <span>{item.icon}</span>
-                    <span className='moda-sptag'>{item.name}</span>
-                 </div>
-              </Link>
-           
-          ))}
-     </div>
-   )
- }
- 
- export default Pages
- 
+  return (
+    <div>
+      {PagesItem.map((item, index) => (
+        <Link
+          className="linke"
+          key={index}
+          to={`${item.path}`}
+          onClick={closeModalHandler}
+        >
+          <div className="pages-moda">
+            <span>{item.icon}</span>
+            <span className="moda-sptag">{item.name}</span>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Pages;
