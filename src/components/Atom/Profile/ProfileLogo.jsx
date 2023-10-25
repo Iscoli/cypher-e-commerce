e02fb4f24d699800755a3bbb0b141a1a2a982b3b
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-10-25 07:56:11 -0700

    profile

--- src/components/Atom/Profile/ProfileLogo.jsx
+++ src/components/Atom/Profile/ProfileLogo.jsx
@@ -1,33 +1,45 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {getAuth} from "firebase/auth";
-import {faUser } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom'
-import { useSelector,useDispatch } from 'react-redux';
-import { useEffect,useState } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
+function Profile() {
+  const [email, setEmail] = useState(null);
+  const auth = getAuth();
 
-    function Profile(){
-        const [email, setEmail] = useState(false)
-        const dispatch = useDispatch()
-        const auth = getAuth();
-
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
         
-        const user = auth.currentUser
-        
-        useEffect(()=>{
-           if(auth.currentUser == null){
-            setEmail(false)
-           }else{
-            setEmail(true)
-           }
-        },[auth.currentUser,email])
-     
-        console.log(email,'hjajw')  
-      
+      if (user) {
+        // User is logged in
+        setEmail(user.email);
+      } else {
+        // User is logged out
+        setEmail(null);
+      }
+    });
 
-    return  <div>
-        {auth.currentUser !== null ? <div className='user-email'><Link to='/DashBoard' className='  link-style'><h1>{user.email.charAt(0)}</h1> </Link></div> : <div> <Link to='/DashBoard' ><FontAwesomeIcon className='svg' icon={faUser}/></Link></div>}
+    return () => unsubscribe(); 
+  }, [auth]);
+
+  return (
+    <div>
+      {email !== null ? (
+        <div className='user-email'>
+          <Link to='/DashBoard' className='link-style'>
+            <h1>{email.charAt(0)}</h1>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to='/DashBoard'>
+            <FontAwesomeIcon className='svg' icon={faUser} />
+          </Link>
+        </div>
+      )}
     </div>
-    }
-    // <div className='user-email'>{ user !== null ?  <Link to='/DashBoard' className='  link-style'><h1>{user.email.charAt(0)}</h1> </Link> :  <Link to='/DashBoard' ><FontAwesomeIcon className='svg' icon={faUser}/></Link>} </div>
-    export default Profile
+  );
+}
+
+export default Profile;
