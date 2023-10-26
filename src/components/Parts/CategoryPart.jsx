a411c2eedaa0bba7710fde934272da511818af83
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-10-26 10:02:13 -0700

    completed CAtegory Part

--- src/components/Parts/CategoryPart.jsx
+++ src/components/Parts/CategoryPart.jsx
@@ -3,26 +3,53 @@ import bread from "../assets/Bread & bakery.webp";
 import fish from "../assets/fish & meat.webp";
 import fresh from "../assets/fresh & natural.webp";
 import styles from "./PartsStyle/CategoryPart.moudle.css";
+import { Link } from "react-router-dom";
 
 function CategoryPart() {
   return (
     <div className="sec1">
-      <div style={{position:'relative'}}>
+      <div style={{ position: "relative" }}>
         <img className="pic1" src={bread} alt="" />
         <div className="pic1Sub">
           <p>Taste of</p>
-          <h3>fresh &amp; natural</h3>
+          <h2>Bread &amp; Backery</h2>
           <p>Weekend discount offer</p>
           <div className="natural-btn">
-            <button className="">shop now</button>
+            <Link to='/main-category/breakfast'>
+              <button className="">shop now</button>
+            </Link>
           </div>
         </div>
       </div>
-      <div>
+      <div
+      style={{position:"relative"}}
+      >
         <img className="pic2" src={fish} alt="" />
+        <div className="pic2Sub">
+          <p>Taste of</p>
+          <h2>fish &amp; meat</h2>
+          <p>Weekend discount offer</p>
+          <div className="natural-btn">
+            <Link to='/main-category/fish-meat'>
+              <button className="">shop now</button>
+            </Link>
+          </div>
+        </div>
       </div>
-      <div>
+      <div
+      style={{position:"relative"}}
+      >
         <img className="pic3" src={fresh} alt="" />
+        <div className="pic3Sub">
+          <p>Taste of</p>
+          <h2>fresh &amp; natural</h2>
+          <p>Weekend discount offer</p>
+          <div className="natural-btn">
+            <Link to='/main-category/fruits-vegetable'>
+              <button className="">shop now</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
