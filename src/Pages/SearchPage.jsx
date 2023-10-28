314978d1b18fdd07091fe7db2c1305cd44ec9210
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-10-28 02:31:45 -0700

    message sent

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import CategoryPart from "../components/Parts/CategoryPart";
 import CategorySwipper from "../components/Parts/CategorySwipper/CategorySwipper";
-import result from  '../components/assets/ba-sakamako.svg';
-import  './PagesStyle/SearchPage.module.css';
+import result from '../components/assets/ba-sakamako.svg';
+import styles from './PagesStyle/SearchPage.module.css'; // Import CSS module correctly
 
 function SearchPage() {
 
@@ -13,34 +13,34 @@ function SearchPage() {
   const query = queryParams.get("query");
 
   useEffect(() => {
-    console.log(query,'ooeo'); 
+    console.log(query, 'ooeo');
   }, [query]);
 
 
-  return <div 
-  style={{ marginTop: "90px" }}
-  className="">
-      <section>
-        <CategoryPart/>
-      </section>
- 
-      <section
-      className={'swipp'}
-      >
-        <CategorySwipper/>
-      </section>
+  return <div
+    style={{ marginTop: "90px" }}
+   >
+    <section>
+      <CategoryPart />
+    </section>
 
-      <section className={'stage3'}>
-        <div className={'resultDiv'}>
-          <img 
-          className={'result'}
-           src={result}
-          />
-        </div>
-        <p
-        style={{textAlign:"center"}}
-        >Sorry, we cannot find this product 😞</p>
-      </section>
+    <div className={styles.cont}>
+    <section>
+      <CategorySwipper />
+    </section>
+
+    <section className={styles.secn3}> {/* Use CSS module class */}
+      <div className={styles.resultDiv}> {/* Use CSS module class */}
+        <img
+          className={styles.resultImg} 
+          src={result}
+        />
+      </div>
+      <p
+        style={{ textAlign: "center" }}
+      >Sorry, we cannot find this product 😞</p>
+    </section>
+    </div>
   </div>;
 }
 
