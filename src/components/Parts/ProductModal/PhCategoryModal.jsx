cc787ceeccc1365505c1d8d3add95654d9081df3
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-10-31 06:55:16 -0700

    last one

--- src/components/Parts/ProductModal/PhCategoryModal.jsx
+++ src/components/Parts/ProductModal/PhCategoryModal.jsx
@@ -6,10 +6,7 @@ import { faClose } from "@fortawesome/free-solid-svg-icons";
 import Logo from "../../Logo/Logo";
 import MainItemModal from "./ItemModal/MainItemModal";
 
-
 function PhCategoryModal(props) {
-  
- 
   return (
     <>
       <div className={props.open ? "main-phm  moveaway " : "main-phm movehere"}>
@@ -31,17 +28,17 @@ function PhCategoryModal(props) {
           props.open ? "main-phmodal  moveaway " : "main-phmodal movehere"
         }
       >
-        <div className="phmodal-flex2"
-         style={{padding:'0.6rem 1.8rem', marginBottom:'10px'
-      }}>
-         <h3>All Categories</h3>
+        <div
+          className="phmodal-flex2"
+          style={{ padding: "0.6rem 1.8rem", marginBottom: "10px" }}
+        >
+          <h3>All Categories</h3>
           <hr></hr>
-        <MainItemModal closeModalHandler={props.closeModalHandler}/>
-         
+          <MainItemModal closeModalHandler={props.closeModalHandler} />
 
           <h3>Pages</h3>
           <hr></hr>
-          <Pages  closeModalHandler={props.closeModalHandler}/>
+          <Pages closeModalHandler={props.closeModalHandler} />
         </div>
       </div>
     </>
