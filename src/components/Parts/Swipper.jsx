195011fa796239722b12c7e9c774613f8ffff2c9
Iscoli <96597856+Iscoli@users.noreply.github.com>
2024-02-05 18:54:09 -0800

    changing of slider image

--- src/components/Parts/Swipper.jsx
+++ src/components/Parts/Swipper.jsx
@@ -5,11 +5,11 @@ import "swiper/swiper-bundle.css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import Basket1 from "../assets/E8.jpg";
-import Basket2 from "../assets/E7.jpg";
-import Basket3 from "../assets/E6.webp";
-import Basket4 from "../assets/E9.jpg";
-import Basket5 from "../assets/E10.png";
+import Basket1 from "../assets/bg-image/bg1.webp";
+import Basket2 from "../assets/bg-image/bg2.jpg";
+import Basket3 from "../assets/bg-image/bg3.webp";
+import Basket4 from "../assets/bg-image/bg4.jpg";
+import Basket5 from "../assets/bg-image/bg5.jpg";
 import "./PartsStyle/Swipper.css";
 import { Link } from "react-router-dom";
 
@@ -34,7 +34,12 @@ function Swipper() {
       >
         <div className="mySwipper-container">
           <SwiperSlide style={{ position: "relative" }}>
-            <img loading="lazy" className="swipper-imgs" src={Basket1} alt="Basket one" />
+            <img
+              loading="lazy"
+              className="swipper-imgs"
+              src={Basket1}
+              alt="Basket one"
+            />
             <div className="sliderText">
               <h2>Quality Freshness Guaranteed</h2>
               <p>
@@ -47,7 +52,12 @@ function Swipper() {
             </div>
           </SwiperSlide>
           <SwiperSlide style={{ position: "relative" }}>
-            <img loading="lazy" className="swipper-imgs" src={Basket2} alt="Basket two" />
+            <img
+              loading="lazy"
+              className="swipper-imgs"
+              src={Basket2}
+              alt="Basket two"
+            />
             <div className="sliderText">
               <h2>Quality Freshness Guaranteed</h2>
               <p>
@@ -60,7 +70,12 @@ function Swipper() {
             </div>
           </SwiperSlide>
           <SwiperSlide style={{ position: "relative" }}>
-            <img loading="lazy" className="swipper-imgs" src={Basket3} alt="Basket three" />
+            <img
+              loading="lazy"
+              className="swipper-imgs"
+              src={Basket3}
+              alt="Basket three"
+            />
             <div className="sliderText">
               <h2>Quality Freshness Guaranteed</h2>
               <p>
@@ -73,7 +88,12 @@ function Swipper() {
             </div>
           </SwiperSlide>
           <SwiperSlide style={{ position: "relative" }}>
-            <img loading="lazy" className="swipper-imgs" src={Basket4} alt="Basket four" />
+            <img
+              loading="lazy"
+              className="swipper-imgs"
+              src={Basket4}
+              alt="Basket four"
+            />
             <div className="sliderText">
               <h2>Quality Freshness Guaranteed</h2>
               <p>
@@ -86,7 +106,12 @@ function Swipper() {
             </div>
           </SwiperSlide>
           <SwiperSlide style={{ position: "relative" }}>
-            <img loading="lazy" className="swipper-imgs" src={Basket5} alt="Basket five" />
+            <img
+              loading="lazy"
+              className="swipper-imgs"
+              src={Basket5}
+              alt="Basket five"
+            />
             <div className="sliderText">
               <h2>Quality Freshness Guaranteed</h2>
               <p>
